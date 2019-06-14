3e0885a8795d1bae0375d261a9dafd6e639629e2
Alexander Vieira da Silva <alexander.vieira@mgn.local>
2019-06-14 18:01:34 -0300

    Initial Commit

---
+++ Infra/Data/Sql/MySql/Contexts/SGCMgnCtx.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MySql.Contexts
+{
+    class SGCMgnCtx : DbContext
+    {
+        public SGCMgnCtx()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseMySql(Properties
+                .Resources.ResourceManager.GetString("NAME_STR_CONN"));
+        }
+    }
+}
